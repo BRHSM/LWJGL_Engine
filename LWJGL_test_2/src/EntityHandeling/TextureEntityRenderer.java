7be1c0ef2bd02ecf0732c5518dacaa236910cbf3
user <user@172.30.18.125>
2018-10-15 13:29:35 +0200

    Started implementing entity

---
+++ LWJGL_test_2/src/EntityHandeling/TextureEntityRenderer.java
@@ -0,0 +1,41 @@
+package EntityHandeling;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+import org.lwjglx.util.vector.Matrix4f;
+
+import GraphicsEngine.AbstractShader;
+import GraphicsEngine.UniformTestShader;
+import Math.MatrixMaths;
+import ModelHandeling.TexturedModel;
+
+public class TextureEntityRenderer {
+	public void prepare() {
+		GL11.glClearColor(0,0,0,1);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+	}
+	/** Render a textured model to the screen.
+	 *
+	 * @param model the model to render.
+	 */
+	
+	// create for each model.
+	public void render (AbstractEntity entity, AbstractShader shader) {
+		TexturedModel model = (TexturedModel)entity.getModel();
+		GL30.glBindVertexArray(model.getVaoID());
+		GL20.glEnableVertexAttribArray(0);
+		GL20.glEnableVertexAttribArray(1);
+		Matrix4f transformationMatrix = MatrixMaths.createTransformationMatrix(entity.getPosition(), entity.getRx(), entity.getRy(), entity.getRz(), entity.getScale());
+		((UniformTestShader)shader).loadTransformationMatrix(transformationMatrix);
+		GL13.glActiveTexture(GL13.GL_TEXTURE0);
+		GL11.glBindTexture(GL11.GL_TEXTURE_2D, ((TexturedModel)entity.getModel()).getTexture().getTextureID());
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, model.getVaoID());
+		GL11.glDrawElements(GL11.GL_TRIANGLES, model.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+		GL20.glDisableVertexAttribArray(0);
+		GL20.glDisableVertexAttribArray(1);
+		GL30.glBindVertexArray(0);
+	}
+}
