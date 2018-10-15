77e852a39a5b80411c1cc45b60bbca2ba35c34b6
Bram Steenbergen <BRHSM@outlook.com>
2018-10-15 18:37:54 +0200

    Added Documentation.

--- LWJGL_test_2/src/Math/MatrixMaths.java
+++ LWJGL_test_2/src/Math/MatrixMaths.java
@@ -2,8 +2,22 @@ package Math;
 
 import org.lwjglx.util.vector.Matrix4f;
 import org.lwjglx.util.vector.Vector3f;
-
+/** Class which holds functions for matrix maths. 
+ * 
+ * @author Bram Steenbergen
+ * @version 1.0
+ * @since 1.0
+ */
 public class MatrixMaths {
+	/** Create a transformationMatrix
+	 * 
+	 * @param translation the positional translation of the model.
+	 * @param rx The X rotation for the model.
+	 * @param ry The Y rotation for the model.
+	 * @param rz The Z rotation for the model.
+	 * @param scale The scale for the model.
+	 * @return The transformationMatrix.
+	 */
 	public static Matrix4f createTransformationMatrix(Vector3f translation, float rx, float ry, float rz, float scale) {
 		Matrix4f matrix = new Matrix4f();
 		matrix.setIdentity();
