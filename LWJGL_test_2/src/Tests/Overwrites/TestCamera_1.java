031965e7747c53b84dc81fd9a666bfd22e3cdf52
Bram Steenbergen <BRHSM@outlook.com>
2018-10-19 16:14:05 +0200

    Added test classes

---
+++ LWJGL_test_2/src/Tests/Overwrites/TestCamera_1.java
@@ -0,0 +1,22 @@
+package Tests.Overwrites;
+
+import org.lwjgl.opengl.GL;
+import org.lwjgl.glfw.GLFW;
+import org.lwjglx.input.Keyboard;
+
+import Engine.DisplayRenderEngine.AbstractCamera;
+import Engine.InputHandeling.KeyStrokeHandler;
+
+public class TestCamera_1 extends AbstractCamera{
+	 public void move(){
+	        if(KeyStrokeHandler.isKeyDown(GLFW.GLFW_KEY_W)){
+	            position.z-=0.02f;
+	        }
+	        if(KeyStrokeHandler.isKeyDown(GLFW.GLFW_KEY_D)){
+	            position.x+=0.02f;
+	        }
+	        if(KeyStrokeHandler.isKeyDown(GLFW.GLFW_KEY_A)){
+	            position.x-=0.02f;
+	        }
+	    }
+}
---
+++ LWJGL_test_2/src/Tests/Tests/Test_7.java
@@ -0,0 +1,63 @@
+package Tests.Tests;
+
+import org.lwjglx.util.vector.Vector3f;
+
+import Engine.Core.DataObject;
+import Engine.Core.Initializer;
+import Engine.EntityHandeling.AbstractEntityStructure;
+import Engine.EntityHandeling.BasicEntityModifier;
+import Engine.ModelHandeling.TexturedModelStructure;
+import Tests.Overwrites.TestCamera_1;
+/** A basic test to see if the engine works with no data provided. This is
+ *  the bare minimum of code for a "game" to launch.
+ * 
+ * @author Bram Steenbergen
+ * @version 1.0
+ * @since 1.0
+ *
+ */
+public class Test_7 {
+	
+	private static Initializer init; 	
+	
+	/** Starts execution.
+	 * @param args terminal arguments
+	*/
+	public static void test() {
+		init = new Initializer();
+		
+		//create a basic model (Trapezoid). 
+		float[] vertices = {
+			    -0.5f, 0.5f, 0f,
+			    -0.5f, -0.5f, 0f,
+			    0.5f, -0.5f, 0f,
+			    0.5f, 0.5f, 0f,
+			  };
+		//create a basic model index list.
+		int[] indexes = {
+				//left Bot
+				0,1,3,
+				//right top
+				3,1,2
+		};
+		
+		float[] textureCoordinates = {
+				0f,0f,
+				0f,1f,
+				1f,1f,
+				1f,0f
+		};
+		
+		//setup modle loader. 
+		TexturedModelStructure modelStructure = new TexturedModelStructure(vertices, textureCoordinates, indexes);
+		AbstractEntityStructure entityStructure = new AbstractEntityStructure(modelStructure, new Vector3f(0, 0, -1), 0, 0, 0, 1);
+		entityStructure.setEntityModifier(new BasicEntityModifier(new Vector3f(0,0,-0.01f),0,0,0,0));
+		
+		//Setup DataObject.
+		DataObject object = new DataObject();
+		object.addEntity(entityStructure);
+		object.setCamera(new TestCamera_1());
+		
+		init.start(object);
+	}
+}
