ee9d38f74e3ad20e08641f29650dd21b816ea8de
Bram Steenbergen <BRHSM@outlook.com>
2018-10-23 14:07:42 +0200

    Added shader chooser class

---
+++ LWJGL_test_2/src/Engine/Graphics/Shaders/ShaderChooser.java
@@ -0,0 +1,45 @@
+package Engine.Graphics.Shaders;
+
+import Engine.Data.OptionManager.EngineOptions;
+import Engine.Data.OptionManager.GraphicOptions;
+import Engine.Data.OptionManager.OptionHandler;
+
+public class ShaderChooser {
+	public static final int VERTEX_TYPE = 301;
+	public static final int FRAGMENT_TYPE = 302;
+	
+	public static String getUsableShaderFileName(int type) {
+		if(OptionHandler.getProperty(EngineOptions.SHADERUSECUSTOM_KEY, OptionHandler.ENGINE_OPTION_ID).equals("1")) {
+			if(type == VERTEX_TYPE) {
+				return OptionHandler.getProperty(EngineOptions.SHADERCUSTOMVERTEXNAME_KEY, OptionHandler.ENGINE_OPTION_ID) + ".vs";
+			}
+			if(type == FRAGMENT_TYPE) {
+				return OptionHandler.getProperty(EngineOptions.SHADERCUSTOMFRAGMENTNAME_KEY, OptionHandler.ENGINE_OPTION_ID) + ".fs";
+			}
+		} else {
+			if(OptionHandler.getProperty(GraphicOptions.USEVIEWMATRIX_KEY, OptionHandler.GRAPHIC_OPTION_ID).equals("1")) {
+				if(type == VERTEX_TYPE) {
+					return OptionHandler.getProperty(EngineOptions.SHADERPRNAME_KEY, OptionHandler.ENGINE_OPTION_ID) + ".vs";
+				}
+				if(type == FRAGMENT_TYPE) {
+					return OptionHandler.getProperty(EngineOptions.SHADERPRNAME_KEY, OptionHandler.ENGINE_OPTION_ID) + ".fs";
+				}
+			}
+			if(OptionHandler.getProperty(GraphicOptions.USEPROJECTIONMARTRIX_KEY, OptionHandler.GRAPHIC_OPTION_ID).equals("1")) {
+				if(type == VERTEX_TYPE) {
+					return OptionHandler.getProperty(EngineOptions.SHADERVINAME_KEY, OptionHandler.ENGINE_OPTION_ID) + ".vs";
+				}
+				if(type == FRAGMENT_TYPE) {
+					return OptionHandler.getProperty(EngineOptions.SHADERVINAME_KEY, OptionHandler.ENGINE_OPTION_ID) + ".fs";
+				}
+			}
+			if(type == VERTEX_TYPE) {
+				return OptionHandler.getProperty(EngineOptions.SHADERDEFAULTNAME_KEY, OptionHandler.ENGINE_OPTION_ID) + ".vs";
+			}
+			if(type == FRAGMENT_TYPE) {
+				return OptionHandler.getProperty(EngineOptions.SHADERDEFAULTNAME_KEY, OptionHandler.ENGINE_OPTION_ID) + ".fs";
+			}
+		}
+		return null;
+	}
+}
